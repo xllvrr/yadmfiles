dfe6daf6312179af940ad5a485745ca44eb51447
xllvrr <dtsa.tan@gmail.com>
2025-08-07 23:07:05 +0800

    Added direct path for OMP

--- .config/fish/conf.d/myabbrs.fish
+++ .config/fish/conf.d/myabbrs.fish
@@ -1,3 +1,4 @@
 abbr -a -- lg lazygit
 abbr -a -- cd z
 abbr -a -- v nvim
+abbr -a -- ex eza -a
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
 	# Initialise zoxide
 	zoxide init fish | source
 	# Initialise oh my posh
-	oh-my-posh init fish --config $HOME/.config/omp/omp.toml | source
+	$HOME/.local/bin/oh-my-posh init fish --config $HOME/.config/omp/omp.toml | source
 	# Start ssh
 	# ssh-add ~/.ssh/github_rsa
 end
