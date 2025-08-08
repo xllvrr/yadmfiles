cfd40befccc223d9707ed0717b9c567269dd89de
xllvrr <dtsa.tan@gmail.com>
2025-08-08 09:34:03 +0800

    Added nvim as EDITOR, added fish paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,18 @@
 if status is-interactive
+
 	# Initialise zoxide
 	zoxide init fish | source
+
 	# Initialise oh my posh
 	$HOME/.local/bin/oh-my-posh init fish --config $HOME/.config/omp/omp.toml | source
+
 	# Start ssh
-	# ssh-add ~/.ssh/github_rsa
+	# ssh-add $HOME/.ssh/github_rsa
+
 end
+
+# Set environment variables
+set -gx EDITOR nvim
+
+# Add path
+fish_add_path $HOME/.local/bin $HOME/repos/scripts
